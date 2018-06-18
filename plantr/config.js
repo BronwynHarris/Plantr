0ff885e6900409d6f2b8902773909eaaac30ffc1
sehailey <sarah.elizabeth.hailey@gmail.com>
2018-06-18 16:57:59 -0400

    Hi

---
+++ plantr/config.js
@@ -0,0 +1,11 @@
+const config = {
+    database: 'plantr',
+    username: 'fsa',
+    password: 'secretPassword123',
+    config: {
+        host: 'localhost',
+        dialect: 'postgres'
+    }
+}
+
+module.exports = config;
